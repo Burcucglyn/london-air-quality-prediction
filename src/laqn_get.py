0fadb7d0c2e98646c8af62ee75af177ca0137d97
Burdzhu Chaglayan <burcucglyn95@gmail.com>
2025-11-18 21:56:54 +0000

    Added url to config.py file. Created the first class laqnGet and first function get_groups to fetch the monitoring groups information. The data will be saved as groups.csv to data/laqn folder.

---
+++ src/laqn_get.py
@@ -0,0 +1,41 @@
+"""I will collect/get LAQN data from the API endpoints defined in config.py and will create functions for each endpoint to fetch the data.
+
+1. get_groups: This function will fetch the available monitoring groups. (groups name London)
+2. get_monitor_sites: This function will fetch monitoring sites for a given group name.
+3. get_hourly_data: This function will fetch hourly air quality data for a specific site and species within a date range.
+"""
+
+#import statements below for necessary libraries to make API requests and handle data.
+import requests
+import json, csv
+
+#config.py file importing Config class to access the API endpoint URLs.
+from config import Config
+import pandas as pd
+
+
+
+class laqnGet:
+    """Class to keep get_groups, get_monitor_sites functions to under one roof."""
+
+    def __init__(self):
+        """Initialize the laqnGet class with Config instance."""
+        self.config = Config()
+
+    def get_groups(self):
+        """Fetch the available monitoring groups from the LAQN API."""
+        url = self.config.get_groups
+        response = requests.get(url)
+        df = pd.DataFrame(response.json()['Groups']['Group'])
+
+        #save to csv file as groups.csv in data/laqn folder
+        df.to_csv('../data/laqn/groups.csv', index=False)
+        if response.status_code == 200:
+            return df
+        else:
+            response.raise_for_status()
+
+    
+            
+
+    
