62f1488eba907da2d3d19c0d286904c4276a654f
Burdzhu Chaglayan <burcucglyn95@gmail.com>
2025-11-18 22:35:50 +0000

    Realized I used the wrong URL to fetch the API, so I'm changing the URL in config.py and deleting the groups.csv file.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     # API Settings
     laqn_url  = "https://api.erg.ic.ac.uk/AirQuality"
-    get_groups = "https://api.erg.ic.ac.uk/AirQuality/Information/Groups/Json"
+    get_groups= "https://api.erg.ic.ac.uk/AirQuality/Annual/MonitoringObjective/GroupName={GROUPNAME}/Json"
     get_monitor_sites = "https://api.erg.ic.ac.uk/AirQuality/Information/MonitoringSiteSpecies/GroupName={GROUPNAME}/Json"
     get_hourly_data = "https://api.erg.ic.ac.uk/AirQuality/Data/SiteSpecies/SiteCode={SITECODE}/SpeciesCode={SPECIESCODE}/StartDate={STARTDATE}/EndDate={ENDDATE}/Period={PERIOD}/Units={UNITS}/Step={STEP}/Json"
     OPENMETEO_BASE_URL = "https://api.open-meteo.com/v1"
