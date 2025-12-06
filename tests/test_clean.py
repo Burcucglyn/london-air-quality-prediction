328de6ba445a4e3a8b85b89d423b844d318bf0ab
Burdzhu Chaglayan <burcucglyn95@gmail.com>
2025-12-06 20:45:11 +0000

    update modules.

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -1,1 +1,14 @@
 """ Module for testing the clean.py functions. """
+
+import unittest
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import tempfile
+import shutil
+from datetime import datetime, timedelta
+
+# project root as a Path
+proj_root = Path(__file__).resolve().parent.parent
+# add project root to sys.path for imports
+sys.path.insert(0, str(proj_root))
